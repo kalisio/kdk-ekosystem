406a4c65c2ed45b0eb07b04771e7ea00add1877a
cnouguier <ch.nouguier@gmail.com>
2026-05-22 10:47:11 +0200

    test: cleaned tests

---
+++ packages/kdk-core-ui/test/index.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('kdk-core-ui:index', () => {
+  it('dummy test', () => {
+    expect(true).toBe(true)
+  })
+})
--- packages/kdk-core-ui/vitest.config.js
+++ packages/kdk-core-ui/vitest.config.js
@@ -11,35 +11,8 @@ export default mergeConfig(baseConfig, defineConfig({
   plugins: [
     vue()
   ],
-  resolve: {
-    alias: {
-      config: path.resolve(__dirname, 'test/config.js'),
-      quasar: path.resolve(__dirname, 'node_modules/quasar/dist/quasar.esm.prod.js'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      'jwt-decode': path.resolve(__dirname, 'node_modules/jwt-decode/build/jwt-decode.cjs.js'),
-      'vue-i18n': path.resolve(__dirname, 'node_modules/vue-i18n/index.js'),
-      'moment-timezone/builds/moment-timezone-with-data-10-year-range.js':
-        path.resolve(__dirname, 'node_modules/moment-timezone/builds/moment-timezone-with-data-10-year-range.js'),
-      'feathers-reactive': path.resolve(__dirname, 'node_modules/feathers-reactive/dist/index.cjs'),
-      '@kalisio/feathers-automerge': path.resolve(__dirname, 'node_modules/@kalisio/feathers-automerge/index.js'),
-      'path-browserify': path.resolve(__dirname, 'node_modules/path-browserify/index.js'),
-      '@thumbmarkjs/thumbmarkjs': path.resolve(__dirname, 'node_modules/@thumbmarkjs/thumbmarkjs/dist/thumbmark.esm.js'),
-      'vue-router': path.resolve(__dirname, 'node_modules/vue-router/dist/vue-router.mjs'),
-      'ajv-i18n': path.resolve(__dirname, 'node_modules/ajv-i18n/localize/index.js')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: { api: 'legacy' },
-      sass: { api: 'legacy' }
-    }
-  },
   test: {
     name: 'core-ui',
-    environment: 'happy-dom',
-    disableConsoleIntercept: true,
-    setupFiles: ['./test/setup.js'],
-    css: false,
-    exclude: ['**/node_modules/**', '**/*.browser.test.js']
+    environment: 'happy-dom'
   }
 }))
