a470a1b70f4a549da2e9bbc7142082c7d9049eab
Luc Claustres <luc.claustres@orange.fr>
2026-09-14 18:04:47 +0200

    wip: Allow setting a default value in serialize hook rules (reported from kdk #1441)

--- packages/kdk-core-api/src/hooks/hooks.model.js
+++ packages/kdk-core-api/src/hooks/hooks.model.js
@@ -49,7 +49,7 @@ export function serialize (rules, options = {}) {
     // Apply the rules for each item
     items.forEach(item => {
       rules.forEach(rule => {
-        let source = _.get(item, rule.source)
+        let source = _.get(item, rule.source, rule.default)
         if (!is.nil(source)) {
           if (rule.filter) source = source.filter(sift(rule.filter))
           _.set(item, rule.target, source)
